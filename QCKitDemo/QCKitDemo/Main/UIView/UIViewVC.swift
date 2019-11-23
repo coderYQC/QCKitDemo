bb470c7c90b99cbed3b8386c186f178470a6f9b2
yqc5521 <595672928@qq.com>
2019-11-23 12:03:40 +0800

    完善了框架

---
+++ QCKitDemo/QCKitDemo/Main/UIView/UIViewVC.swift
@@ -0,0 +1,30 @@
+//
+//  UIViewVC.swift
+//  QCKitDemo
+//
+//  Created by yanqunchao on 2019/11/23.
+//  Copyright © 2019 yanqunchao. All rights reserved.
+//
+
+import UIKit
+
+class UIViewVC: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
