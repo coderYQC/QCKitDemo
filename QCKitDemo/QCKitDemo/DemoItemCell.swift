25d970d07c711e4de2fa88a90e3640437fd085a4
yqc5521 <595672928@qq.com>
2019-12-02 11:14:19 +0800

    添加了部分demo

---
+++ QCKitDemo/QCKitDemo/DemoItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  DemoItemCell.swift
+//  QCKitDemo
+//
+//  Created by yanqunchao on 2019/12/2.
+//  Copyright © 2019 yanqunchao. All rights reserved.
+//
+
+import UIKit
+
+class DemoItemCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ QCKitDemo/QCKitDemo/Main/DispatchGroup/DispatchGroupVC.swift
@@ -0,0 +1,30 @@
+//
+//  DispatchGroupVC.swift
+//  QCKitDemo
+//
+//  Created by yanqunchao on 2019/12/2.
+//  Copyright © 2019 yanqunchao. All rights reserved.
+//
+
+import UIKit
+
+class DispatchGroupVC: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
